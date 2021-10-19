c6f4905fad17e355987a202aaacef7a79694b47c
David <r.d.jorgensen@gmail.com>
2021-10-19 02:34:37 -0700

    base framework

---
+++ main.js
@@ -0,0 +1,12 @@
+require('dotenv').config({ path: '.env' });
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Test Message');
+});
+
+app.listen(process.env.WEB_PORT, () => {
+  console.log(`Server is listning on ${process.env.WEB_PORT}.`);
+});
