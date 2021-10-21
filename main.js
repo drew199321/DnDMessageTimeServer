bf73748e823a58937fc6cc3516b5a2ce0d24c733
David <r.d.jorgensen@gmail.com>
2021-10-21 11:41:03 -0700

    db connection and lint added

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
+
+dbConnection.connect((err) => {
+  if (err) {
+    console.error(`Database connection failed:\n${err.stack}`);
+    return;
+  }
+  console.log('Connected to database.');
+});
 
 app.get('/', (req, res) => {
   res.send('Test Message');
