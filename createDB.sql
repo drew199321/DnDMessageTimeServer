03aa4aac614245e3c49b2060342ad572e4a54a3a
David <r.d.jorgensen@gmail.com>
2021-10-25 14:03:12 -0700

    DB removed

--- createDB.sql
+++
@@ -1,7 +0,0 @@
--- version 0.1.0
-drop database if exists dnd_db;
-create database dnd_db;
-use dnd_db;
-
-set names utf8mb4;
-set character_set_client = utf8mb4;
--- main.js
+++ main.js
@@ -1,29 +1,12 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
-const mysql = require('mysql');
 
 const app = express();
-
-const dbConnection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
-
-dbConnection.connect((err) => {
-  if (err) {
-    console.error(`Database connection failed:\n${err.stack}`);
-    return;
-  }
-  console.log('Connected to database.');
-});
 
 app.get('/', (req, res) => {
   res.send('Test Message');
 });
 
 app.listen(process.env.WEB_PORT, () => {
-  console.log(`Server is listning on ${process.env.WEB_PORT}.`);
+  console.log(`Server is listning on port ${process.env.WEB_PORT}.`);
 });
