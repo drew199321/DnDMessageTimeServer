21a15922a9b77e61e6ccb5dcee9c2478cbc0fbfd
David <r.d.jorgensen@gmail.com>
2021-10-28 05:41:55 -0700

    user registration

--- main.js
+++ main.js
@@ -3,15 +3,30 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
+
+function authetication(data) {
+  // TODO: replace stub with actual auth system
+  console.log(`creating authentication using user data form ${data.username}`);
+  const authToken = { isAuthenticated: true, token: 'tokenData' };
+  return authToken;
+}
 
 app.get('/login', (req, res) => {
   // TODO: Check that user data was sent
   // TODO: Check user against known users and Auth user
   console.log(`user, ${req.query.username} logged in`); // TODO: log the failure as well
 
-  const authData = { isAuthenticated: true, token: 'tokenData' }; // TODO: replace stub with actual auth system
-  res.json(authData);
+  res.json(authetication(req.query)).status(200);
+});
+
+app.post('/register', (req, res) => {
+  // TODO: Check that user data was sent
+  // TODO: Register user in system
+  // TODO: If username exists respond with bad authetication
+  console.log(`user, ${req.body.username} registered`);
+  res.json(authetication(req.body)).status(200);
 });
 
 app.listen(process.env.WEB_PORT, () => {
