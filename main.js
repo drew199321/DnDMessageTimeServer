e620357146bc2e623e401c04a121a6656196e439
David <r.d.jorgensen@gmail.com>
2021-10-28 00:34:58 -0700

    login route

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Test Message');
+app.get('/login', (req, res) => {
+  // TODO: Check that user data was sent
+  // TODO: Check user against known users and Auth user
+  console.log(`user, ${req.query.username} logged in`); // TODO: log the failure as well
+
+  const authData = { isAuthenticated: true, token: 'tokenData' }; // TODO: replace stub with actual auth system
+  res.json(authData);
 });
 
 app.listen(process.env.WEB_PORT, () => {
