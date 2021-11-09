c9aceac62d4eff71926e5da8395ab7d0fb67fddf
David <r.d.jorgensen@gmail.com>
2021-11-08 18:50:52 -0800

    typo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
-function authetication(data) {
+function authentication(data) {
   // TODO: replace stub with actual auth system
   console.log(`creating authentication using user data form ${data.username}`);
   const authToken = { isAuthenticated: true, token: 'tokenData' };
@@ -14,22 +14,22 @@ function authetication(data) {
 }
 
 app.get('/login', (req, res) => {
-  // DONE: Check that user data was sent
+  // Check that user data was sent
   if (req) {
     console.log(`user, ${req.query.username} logged in`);
-    res.json(authetication(req.query)).status(200);
+    res.json(authentication(req.query)).status(200);
   } else {
     console.log('Error: Credentials not recieved from client');
-    res.json(authetication(req.query)).status(300);
+    res.json(authentication(req.query)).status(300);
   }
 });
 
 app.post('/register', (req, res) => {
   // TODO: Check that user data was sent
   // TODO: Register user in system
-  // TODO: If username exists respond with bad authetication
+  // TODO: If username exists respond with bad authentication
   console.log(`user, ${req.body.username} registered`);
-  res.json(authetication(req.body)).status(200);
+  res.json(authentication(req.body)).status(200);
 });
 
 app.listen(process.env.WEB_PORT, () => {
