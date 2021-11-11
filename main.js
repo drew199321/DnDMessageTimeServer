636ce14a7fdecdd3a8e3f011d97ac9125499d373
David <r.d.jorgensen@gmail.com>
2021-11-10 19:08:17 -0800

    http spicific server and more TODOs

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 
 const app = express();
+const server = http.createServer(app);
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
@@ -15,23 +17,28 @@ function authentication(data) {
 
 app.get('/login', (req, res) => {
   // Check that user data was sent
-  if (req) {
+  console.log(req.query);
+  if (req) { // TODO: Check that there is a username and password in query object and query object exits
+    // TODO: Check file system for user with those username and password combo
+    // TODO: check user is member of group as well
     console.log(`user, ${req.query.username} logged in`);
     res.json(authentication(req.query)).status(200);
   } else {
     console.log('Error: Credentials not recieved from client');
-    res.json(authentication(req.query)).status(300);
+    res.json(authentication(req.query)).status(300); // TODO: should respond with isAuth as false and null for token
   }
 });
 
 app.post('/register', (req, res) => {
+  console.log(req.body);
   // TODO: Check that user data was sent
-  // TODO: Register user in system
   // TODO: If username exists respond with bad authentication
+  // TODO: Register user in system
+  // TODO: Register user with the group or create group if user is admin... there can only be one admin
   console.log(`user, ${req.body.username} registered`);
   res.json(authentication(req.body)).status(200);
 });
 
-app.listen(process.env.WEB_PORT, () => {
+server.listen(process.env.WEB_PORT, () => {
   console.log(`Server is listning on port ${process.env.WEB_PORT}.`);
 });
