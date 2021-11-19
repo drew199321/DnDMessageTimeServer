ad3cdf0198238b1caa4f2d7f29c6cda5c7efbfbc
drew199321 <43482659+drew199321@users.noreply.github.com>
2021-11-18 20:48:45 -0800

    updated database with innodb

--- databasecode.sql
+++ databasecode.sql
@@ -7,18 +7,16 @@ set character_set_client = utf8mb4;
 
 create table users(
 	username varchar(255) not null,
-    userid int(255) not null auto_increment,
+    userid int(255) not null auto_increment primary key,
 	passwrd varchar(255) not null,
-    usertype int(255),
-    primary key(userid)
-) engine=storage_engine auto_increment=1 ;
+    usertype int(255)
+) engine=innodb auto_increment=1 ;
 
 create table message(
-	msgid varchar(255) not null,
+	msgid varchar(255) not null primary key,
     msgtime datetime not null,
     msgcontent varchar(255),
 	userid int(255) not null,
-	primary key(msgid),
 	foreign key (userid) references users(userid)
-) engine=storage_engine auto_increment=1 ;
+) engine=innodb auto_increment=1 ;
 
