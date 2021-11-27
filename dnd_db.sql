d359e47112b7d66a90cd40d8ce78b42c30506da0
David <r.d.jorgensen@gmail.com>
2021-11-26 21:32:27 -0800

    int length default and no more files

--- dnd_db.sql
+++ dnd_db.sql
@@ -6,17 +6,16 @@ set names utf8mb4;
 set character_set_client = utf8mb4;
 
 create table users(
-    userid int(255) not null auto_increment primary key,
+	userid int not null auto_increment primary key,
 	username varchar(255) not null,
 	passwrd varchar(255) not null,
-    usertype int(255)
+	usertype int
 ) engine=innodb auto_increment=1 ;
 
 create table messages(
 	msgid varchar(255) not null primary key,
-    msgtime datetime not null,
-    msgcontent varchar(255),
-	userid int(255) not null,
+	msgtime datetime not null,
+  msgcontent varchar(255),
+	userid int not null,
 	foreign key (userid) references users(userid)
 ) engine=innodb auto_increment=1 ;
-
